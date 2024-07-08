fa5e6a61f0789598af9dbd5e4e654a4a8cfbdbad
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-09 01:20:54 +0200

    add: update blogs test manager

--- test/utils/test.manager.blogs.ts
+++ test/utils/test.manager.blogs.ts
@@ -1,57 +1,18 @@
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
+import { TestManagerBase } from './base/test.manager.base';
+import { UserOutputModel } from '../../src/features/users/api/admin/models/user.ouput.model';
+import { credentialsType, testsEndPoints, validAdminCredentials } from '../common/tests.settings';
+import { ITestsCreateModel } from './base/tests.create.model.interface';
+import { TestsCreateBlogModel } from '../e2e.tests/blogs/dataset/blogs.models';
 import { BlogOutputDto } from '../../src/features/blogs/blogs/types/output';
 
-export class BlogsTestManager {
+export class BlogsTestManager extends TestManagerBase<BlogOutputDto> {
   constructor(
     protected readonly app: INestApplication,
-    private accessData: any = {
-      value: '',
-      user: 'admin',
-      password: 'qwerty',
-      options: { type: 'basic' },
-    },
-  ) {}
-
-  createMany = async (numberOfBlogs: number) => {
-    const blogs: Array<BlogOutputDto> = [];
-
-    for (let i = 1; i <= numberOfBlogs; i++) {
-      const createBlogData = {
-        name: 'Blog_' + i,
-        description: 'some valid description',
-        websiteUrl: 'http://www.validurl.com',
-      };
-
-      const res = await this.createOne(createBlogData);
-
-      blogs.push(res.body);
-    }
-    return blogs.reverse();
-  };
-
-  async createOne(createBlogDto: any) {
-    return await request(this.app.getHttpServer()).post('/blogs/').auth(this.accessData.user, this.accessData.password).send(createBlogDto);
-  }
-
-  async updateOne(updateBlogDto: any, blogId: string) {
-    return await request(this.app.getHttpServer())
-      .put('/blogs/' + blogId)
-      .auth(this.accessData.user, this.accessData.password)
-      .send(updateBlogDto);
-  }
-
-  async getOne(blogId: string) {
-    return await request(this.app.getHttpServer()).get('/blogs/' + blogId);
-  }
-
-  async getAll() {
-    return await request(this.app.getHttpServer()).get('/blogs/');
-  }
-
-  async deleteOne(blogId: string) {
-    return await request(this.app.getHttpServer())
-      .delete('/blogs/' + blogId)
-      .auth(this.accessData.user, this.accessData.password);
+    protected accessData: credentialsType = validAdminCredentials,
+    protected createModel: ITestsCreateModel = new TestsCreateBlogModel(),
+    public endPoint: string = testsEndPoints.blogsAdmin,
+  ) {
+    super(app, accessData, createModel, endPoint);
   }
 }
